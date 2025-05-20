cf626e09c53d6e263f7ad793f9f007a2457503c0
our ame <you@example.com>
2025-05-20 23:09:23 +0300

    update file masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,17 +1,22 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и маскирует его"""
-    card_number = card_number.replace(" ", "")
+    if card_number == ' ':
+        return "Пустая строка. Введите номер карты"
+    card_number_split = card_number.split()
+    card_name = card_number_split[:-1]
+    card_num = card_number_split[-1]
+
     # Проверяем хватает цифр в номере карты
-    if len(card_number) != 16:
+    if len(card_num) != 16:
         return "Введен неверный номер карты"
 
     result = []
     # Список для хранения замаскированного номера карты
     counter = 0
     # Счетчик цифр в номере карты, чтобы знать, какую заменить на *
-    for number in card_number:
+    for number in card_num:
         counter += 1
-        if 6 < counter <= len(card_number) - 4:
+        if 6 < counter <= len(card_num) - 4:
             result.append("*")
         else:
             result.append(number)
@@ -28,7 +33,13 @@ def get_mask_card_number(card_number: str) -> str:
 def get_mask_account(card_account: str) -> str:
     """Функция принимает номер счета и маскирует его,
     видны только последние 4 цифры номера, а перед ними **"""
-    card_account = card_account.replace(" ", "")
-    last_part = str(card_account[-4:])
-    return f"**{last_part}"
+    if card_account == ' ':
+        return "Пустая строка. Введите номер счета"
+    card_account_split = card_account.split()
+    account_name = card_account_split[0]
+    account_num = card_account_split[-1]
+    if len(account_num) != 20:
+        return "Введен неверный номер счета"
 
+    last_part = str(account_num[-4:])
+    return f"{account_name} **{last_part}"
